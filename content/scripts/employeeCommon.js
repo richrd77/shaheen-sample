2962705e8372fc4739fbf0100b873014cc7b1400
richrd77@gmail.com <richrd77@gmail.com>
2023-03-12 11:32:47 +0530

    responsive admision form

--- content/scripts/employeeCommon.js
+++ content/scripts/employeeCommon.js
@@ -15,24 +15,7 @@ function PopulateScroll() {
     const scroller = document.createElement('button');
     scroller.id = 'scroller';
     scroller.innerHTML = '<span style="font-size:1.5rem" class="material-symbols-outlined">arrow_upward</span>';
-    scroller.style.position = 'fixed';
-    scroller.style.bottom = '2em';
-    scroller.style.right = '3em';
-    scroller.style.padding = '0.5em';
-    scroller.style.backgroundColor = 'red';
-    scroller.style.color = 'white';
-    scroller.style.borderRadius = '50%';
-    scroller.style.height = 'calc(1.5rem + 1.5em)';
-    scroller.style.width = 'calc(1.5rem + 1.5em)';
     scroller.title = 'Scroll Up';
-    scroller.style.cursor = 'pointer';
-    scroller.style.boxShadow = '0 0 8px 0 rgba(0, 0, 0, 0.5)';
-    scroller.style.opacity = '0';
-    scroller.style.transition = 'opacity 0.5s';
-    scroller.style.userSelect = 'none';
-    scroller.style.pointerEvents = 'none';
-    scroller.style.outline = 'none';
-    scroller.style.border = 'transparent';
     scroller.onclick = () => scroll(0, 0);
     document.body.appendChild(scroller);
 }
@@ -47,13 +30,6 @@ window.onscroll = () => {
     }
 };
 
-const ShowScoller = () => {
-    const s = document.querySelector('#scroller');
-    s.style.opacity = '1';
-    s.style.pointerEvents = 'all';
-};
-const HideScoller = () => {
-    const s = document.querySelector('#scroller');
-    s.style.opacity = '0';
-    s.style.pointerEvents = 'none';
-};
+const ShowScoller = () => document.querySelector('#scroller').classList.add('show');
+
+const HideScoller = () => document.querySelector('#scroller').classList.remove('show');
